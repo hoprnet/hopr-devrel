231a6ed33ff07a06d1d1c85e4ceac121c1f812c1
MartinBenediktBusch <bene.busch1992@gmail.com>
2023-08-23 10:57:54 +0200

    added wrapper around the initialization of account balances

--- subgraph/packages/subgraph-dufour/src/mHoprToken.ts
+++ subgraph/packages/subgraph-dufour/src/mHoprToken.ts
@@ -2,25 +2,22 @@ import { BigInt } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/mHoprToken/mHoprToken";
 import { AccountBalance } from "../generated/schema";
 
-// TODO: please check type for inputs and return values
-export function initiateAccountBalance(accountAddress: string): AccountBalance {
-  let account = new AccountBalance(accountAddress);
-  account.wxHOPRbalance = BigInt.fromI32(0);
-  account.mHOPRbalance = BigInt.fromI32(0);
-  return account
+export const getOrInitializeAccountBalance = (accountAddress: string): AccountBalance => {
+  let account = AccountBalance.load(accountAddress);
+  if (!account) {
+    account = new AccountBalance(accountAddress);
+    account.wxHOPRbalance = BigInt.fromI32(0);
+    account.mHOPRbalance = BigInt.fromI32(0);
+    account.save()
+  }
+  return account;
 }
 
 export function handleTransfer_mHopr(event: Transfer): void {
   // update balance for `from` account
-  let fromAccount = AccountBalance.load(event.params.from.toHex());
-  if (fromAccount == null) {
-    fromAccount = initiateAccountBalance(event.params.from.toHex());
-  }
+  let fromAccount = getOrInitializeAccountBalance(event.params.from.toHex());
   // update balance for `to` account
-  let toAccount = AccountBalance.load(event.params.to.toHex());
-  if (toAccount == null) {
-    toAccount = initiateAccountBalance(event.params.to.toHex());
-  }
+  let toAccount = getOrInitializeAccountBalance(event.params.to.toHex());
 
   // Update the balances based on the transfer event
   let value = event.params.value;
@@ -33,4 +30,3 @@ export function handleTransfer_mHopr(event: Transfer): void {
   fromAccount.save();
   toAccount.save();
 }
-
--- subgraph/packages/subgraph-dufour/src/wxHoprToken.ts
+++ subgraph/packages/subgraph-dufour/src/wxHoprToken.ts
@@ -2,25 +2,22 @@ import { BigInt } from "@graphprotocol/graph-ts";
 import { Transfer } from "../generated/wxHoprToken/wxHoprToken";
 import { AccountBalance } from "../generated/schema";
 
-// TODO: please check type for inputs and return values
-export function initiateAccountBalance(accountAddress: string): AccountBalance {
-  let account = new AccountBalance(accountAddress);
-  account.wxHOPRbalance = BigInt.fromI32(0);
-  account.mHOPRbalance = BigInt.fromI32(0);
-  return account
+export const getOrInitializeAccountBalance = (accountAddress: string): AccountBalance => {
+  let account = AccountBalance.load(accountAddress);
+  if (!account) {
+    account = new AccountBalance(accountAddress);
+    account.wxHOPRbalance = BigInt.fromI32(0);
+    account.mHOPRbalance = BigInt.fromI32(0);
+    account.save()
+  }
+  return account;
 }
 
 export function handleTransfer_wxHopr(event: Transfer): void {
   // update balance for `from` account
-  let fromAccount = AccountBalance.load(event.params.from.toHex());
-  if (fromAccount == null) {
-    fromAccount = initiateAccountBalance(event.params.from.toHex());
-  }
+  let fromAccount = getOrInitializeAccountBalance(event.params.from.toHex());
   // update balance for `to` account
-  let toAccount = AccountBalance.load(event.params.to.toHex());
-  if (toAccount == null) {
-    toAccount = initiateAccountBalance(event.params.to.toHex());
-  }
+  let toAccount = getOrInitializeAccountBalance(event.params.to.toHex());
 
   // Update the balances based on the transfer event
   let value = event.params.value;
