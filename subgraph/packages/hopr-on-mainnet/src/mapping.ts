069fb0b0e3776b15a33b45ce56edaa588af722c9
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2024-09-11 11:28:52 +0200

    subgraph

---
+++ subgraph/packages/hopr-on-mainnet/src/mapping.ts
@@ -0,0 +1,20 @@
+import {
+  Transfer as HoprTransferEvent,
+} from "../generated/HoprToken/HoprToken"
+import { ADDRESS_ZERO, updateHoprAccount } from "./library"
+
+
+export function handleHoprTransfer(event: HoprTransferEvent): void {
+  let addrFrom = event.params.from
+  let addrTo = event.params.to
+  let blkNum = event.block.number
+  let blkTimeStamp = event.block.timestamp
+
+  if (addrFrom.toHex() != ADDRESS_ZERO) {
+    updateHoprAccount(addrFrom, event.params.value, false, blkNum, blkTimeStamp)
+  }
+  if (addrTo.toHex() != ADDRESS_ZERO) {
+    updateHoprAccount(addrTo, event.params.value, true, blkNum, blkTimeStamp)
+  }
+}
+
